855cf9b654df46371972941c33bb352121bdcdfa
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-01-09 20:35:56 +0300

    Added icon files, asset files and more cleaning up

---
+++ src/Assets/assets.tsx
@@ -0,0 +1,37 @@
+import Airplane from "./Airplane.svg";
+import Award from "./Award.svg";
+import Bulb from "./Bulb.svg";
+import Clock from "./Clock.svg";
+import Close from "./close.svg";
+import Coin from "./Coin.svg";
+import Location from "./Location.svg";
+import Logo from "./Logo1.png";
+import Mail from "./Mail.svg";
+import Paper from "./Paper.svg";
+import Person from "./Person.svg";
+import PersonWithPlusIcon from "./PersonWithPlusIcon.svg";
+import Phone from "./Phone.svg";
+import PlusIcon from "./PlusIcon.svg";
+import Rocket from "./Rocket.svg";
+import Tick from "./Tick.svg";
+import Wallets from "./Wallets.svg";
+
+export const assets = {
+  Award,
+  Airplane,
+  Bulb,
+  Clock,
+  Close,
+  Coin,
+  Location,
+  Logo,
+  Mail,
+  Paper,
+  Person,
+  PersonWithPlusIcon,
+  Phone,
+  PlusIcon,
+  Rocket,
+  Tick,
+  Wallets,
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryOne: "#5236FF",
+        primaryTwo: "#242331",
+        secondaryOne: "#F9F9F9",
+        secondaryTwo: "#EFECFF",
+        secondaryThree: "#242331",
+        secondaryFour: "#797979",
+        secondaryFive: "#797979",
+      },
+    },
   },
   plugins: [],
-}
+};
