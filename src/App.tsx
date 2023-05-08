f8a44b3d0ef36641ce345495377d811db2190864
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-05-08 11:39:43 +0300

    Force push

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from "./Components/Footer";
 import Navbar from "./Components/NavBar/Navbar";
 
 function App() {
+  console.log(process.env.REACT_APP_FIREBASE_API_KEY);
   return (
     <div className="relative py-14 w-screen overflow-x-hidden">
       <AnimatePresence>
