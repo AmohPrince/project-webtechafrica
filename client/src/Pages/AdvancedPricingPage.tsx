17171af4adfad6e0e644c4a01202967388239153
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-06-12 15:20:37 +0300

    a comma mahn

--- client/src/Pages/AdvancedPricingPage.tsx
+++ client/src/Pages/AdvancedPricingPage.tsx
@@ -66,7 +66,7 @@ const AdvancedPricingPage = () => {
                 className="w-5 h-5 mr-5"
               />
               <p className="default-paragraph">
-                {feature.replace(/<\/?sp>/g, "")}
+                {feature.replace(/<\/?sp>|,/g, "")}
               </p>
             </div>
           ))}
