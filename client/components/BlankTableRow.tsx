01f3576b951d705bd6ba190af636c6345c98bbf8
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-06-22 21:00:27 +0300

    im exhausted

--- client/components/BlankTableRow.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const BlankTableRow = ({ name }: { name: string }) => {
-  return (
-    <div className="py-5 px-14 text-primaryOne text-sm rounded-sm">{name}</div>
-  );
-};
