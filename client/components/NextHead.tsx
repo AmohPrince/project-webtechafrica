334562e00e699448ce88dd2bff32a85d15da1b98
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-06-26 11:51:42 +0300

    Uploaded favicons

--- client/components/NextHead.tsx
+++ client/components/NextHead.tsx
@@ -31,7 +31,25 @@ export const NextHead = ({
       <meta name="twitter:description" content={twitterDescription} />
       <meta name="twitter:image" content="/logowithdescription.png" />
       <link rel="canonical" href={canonical} />
-      <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
       <script type="application/ld+json">
         {JSON.stringify(schemaJSON ?? {})}
       </script>
